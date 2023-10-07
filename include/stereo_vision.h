0fc41a2b6b5b9ca9b7fa1e36245910e5d33b87a7
shun74 <syunnsyunn74@gmai.com>
2023-10-07 19:04:13 +0900

    cpu streo_vision_processor udpate

--- include/stereo_vision.h
+++ include/stereo_vision.h
@@ -2,7 +2,8 @@
 
 #include <iostream>
 #include <shared_mutex>
-#include <thread>
+#include <atomic>
+#include <future>
 #include <opencv2/opencv.hpp>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -16,16 +17,34 @@
 class StereoVisionProcessor
 {
   private:
-    // data to share among threads 
-    std::shared_mutex mtx_img_, mtx_disp_, mtx_pcd_;
-    cv::Mat img_, left_, right_, disp_, rgb_;
-    pcl::PointCloud<pcl::PointXYZRGB> pcd_;
+    // Image update thread
+    std::shared_mutex mtx_img_;
+    cv::Mat img_, left_, right_;
+    std::atomic<bool> stop_img_update_thread_;
+    std::atomic<bool> is_video_cap_open_;
+    std::future<void> fut_img_update_thread_;
 
-    // thread & stop flag
-    std::thread th_img_update_, th_disp_, th_pcd_, th_viewer_, th_pcd_viewer_;
-    bool stop_img_update_, stop_disp_, stop_pcd_, stop_viewer_, stop_pcd_viewer_;
-    bool up_img_update_, up_disp_, up_pcd_, up_viewer_, up_pcd_viewer_;
-    bool video_cap_;
+    // Compute disparity thread
+    std::shared_mutex mtx_disp_;
+    cv::Mat disp_;
+    std::atomic<bool> stop_disp_thread_;
+    std::future<void> fut_disp_thread_;
+
+    // Compute point cloud thread
+    std::shared_mutex mtx_pcd_;
+    cv::Mat rgb_;
+    pcl::PointCloud<pcl::PointXYZRGB> pcd_;
+    std::atomic<bool> stop_pcd_thread_;
+    std::future<void> fut_pcd_thread_;
+
+    // Images Viewer thread
+    std::atomic<bool> stop_viewer_thread_;
+    std::future<void> fut_viewer_thread_;
+
+    // PCD Viewer thread
+    std::atomic<bool> stop_pcd_viewer_thread_;
+    std::future<void> fut_pcd_viewer_thread_;
+
     
     // basic params
     std::vector<cv::Mat> left_maps_, right_maps_;
--- src/stereo_vision.cpp
+++ src/stereo_vision.cpp
@@ -1,42 +1,50 @@
 #include <chrono>
 #include <stereo_vision.h>
 
-StereoVisionProcessor::StereoVisionProcessor(std::string path, std::vector<cv::Mat> left_maps, std::vector<cv::Mat> right_maps)
-    : stereo_matcher_(path), pcd_generator_(path)
+using namespace std::chrono_literals;
+
+StereoVisionProcessor::StereoVisionProcessor(std::string config_path, std::vector<cv::Mat> left_maps, std::vector<cv::Mat> right_maps)
+  : stereo_matcher_(config_path),
+    pcd_generator_(config_path),
+    left_maps_(left_maps),
+    right_maps_(right_maps),
+    is_video_cap_open_(false),
+    stop_img_update_thread_(true),
+    stop_disp_thread_(true),
+    stop_pcd_thread_(true),
+    stop_viewer_thread_(true),
+    stop_pcd_viewer_thread_(true)
 {
-  video_cap_ = false;
-  stop_img_update_ = true;
-  up_img_update_ = false;
-  stop_disp_ = true;
-  up_disp_ = false;
-  stop_pcd_ = true;
-  up_pcd_ = false;
-  stop_viewer_ = true;
-  up_viewer_ = false;
-  stop_pcd_viewer_ = true;
-  up_pcd_viewer_ = false;
-
-  left_maps_ = left_maps;
-  right_maps_ = right_maps;
-
-  config::StereoVisionParams sv_params(path);
+  config::StereoVisionParams sv_params(config_path);
   setParams(sv_params);
+
+  std::promise<void> img_update_prom, disp_prom, pcd_prom, viewer_prom, pcd_viewer_prom;
+  fut_img_update_thread_  = img_update_prom.get_future();
+  fut_disp_thread_        = disp_prom.get_future();
+  fut_pcd_thread_         = pcd_prom.get_future();
+  fut_viewer_thread_      = viewer_prom.get_future();
+  fut_pcd_viewer_thread_  = pcd_viewer_prom.get_future();
+  img_update_prom.set_value();
+  disp_prom.set_value();
+  pcd_prom.set_value();
+  viewer_prom.set_value();
+  pcd_viewer_prom.set_value();
 }
 
 void StereoVisionProcessor::setParams(config::StereoVisionParams sv_params)
 {
-  img_size_ = sv_params.img_size;
-  win_size_ = sv_params.win_size;
-  img_update_sleep_ = sv_params.img_update_sleep;
-  viewer_update_sleep_ = sv_params.viewer_update_sleep;
-  source_viewer_ = sv_params.source_viewer;
-  rectified_viewer_ = sv_params.rectified_viewer;
-  disparity_viewer_ = sv_params.disparity_viewer;
-  point_cloud_viewer_ = sv_params.point_cloud_viewer;
+  img_size_             = sv_params.img_size;
+  win_size_             = sv_params.win_size;
+  img_update_sleep_     = sv_params.img_update_sleep;
+  viewer_update_sleep_  = sv_params.viewer_update_sleep;
+  source_viewer_        = sv_params.source_viewer;
+  rectified_viewer_     = sv_params.rectified_viewer;
+  disparity_viewer_     = sv_params.disparity_viewer;
+  point_cloud_viewer_   = sv_params.point_cloud_viewer;
   if (point_cloud_viewer_)
   {
-    points_size_ = sv_params.points_size;
-    coordinate_system_ = sv_params.coordinate_system;
+    points_size_        = sv_params.points_size;
+    coordinate_system_  = sv_params.coordinate_system;
   }
 }
 
@@ -51,18 +59,17 @@ void StereoVisionProcessor::updateImage(int video_cap)
   cv::VideoCapture cap(video_cap);
   if (!cap.isOpened())
   {
-    video_cap_ = false;
+    is_video_cap_open_.store(false);
     return;
   }
-  video_cap_ = true;
-  up_img_update_ = true;
+  is_video_cap_open_.store(true);
 
   cap.set(cv::CAP_PROP_BUFFERSIZE, 5);
   cap.set(cv::CAP_PROP_FRAME_WIDTH, img_size_.width * 2);
   cap.set(cv::CAP_PROP_FRAME_HEIGHT, img_size_.height);
 
   // get frame from cap then convert with map
-  while (!stop_img_update_)
+  while (!stop_img_update_thread_.load())
   {
     {
       std::lock_guard<std::shared_mutex> lock(mtx_img_);
@@ -75,16 +82,13 @@ void StereoVisionProcessor::updateImage(int video_cap)
     std::this_thread::sleep_for(std::chrono::milliseconds(img_update_sleep_));
   }
   cap.release();
-  video_cap_ = false;
-  up_img_update_ = false;
-  stop_img_update_ = false;
+  is_video_cap_open_.store(false);
 }
 
 void StereoVisionProcessor::computeDisparity()
 {
-  up_disp_ = true;
   cv::Mat left, right, disp, left_cp;
-  while (!stop_disp_)
+  while (!stop_disp_thread_.load())
   {
     {
       std::shared_lock<std::shared_mutex> lock(mtx_img_);
@@ -102,17 +106,14 @@ void StereoVisionProcessor::computeDisparity()
       left_cp.copyTo(rgb_);
     }
   }
-  up_disp_ = false;
-  stop_disp_ = false;
 }
 
 void StereoVisionProcessor::computePointCloud()
 {
-  up_pcd_ = true;
   cv::Mat color, disp;
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcd(new pcl::PointCloud<pcl::PointXYZRGB>);
 
-  while (!stop_pcd_)
+  while (!stop_pcd_thread_.load())
   {
     {
       std::shared_lock<std::shared_mutex> lock(mtx_disp_);
@@ -123,22 +124,17 @@ void StereoVisionProcessor::computePointCloud()
     pcd_generator_.computePointCloud(color, disp, pcd);
 
     if (pcd->empty())
-    {
-      continue;
-    }
+        continue;
     {
       std::lock_guard<std::shared_mutex> lock(mtx_pcd_);
       pcl::copyPointCloud(*pcd, pcd_);
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(img_update_sleep_));
   }
-  up_pcd_ = false;
-  stop_pcd_ = false;
 }
 
 void StereoVisionProcessor::updateViewer()
 {
-  up_viewer_ = true;
   cv::Mat src, left, right, rect, disp, disp_norm;
 
   if (source_viewer_)
@@ -157,7 +153,7 @@ void StereoVisionProcessor::updateViewer()
       cv::resizeWindow("disparity", win_size_.width / 2, win_size_.height);
   }
 
-  while (!stop_viewer_)
+  while (!stop_viewer_thread_.load())
   {
     {
       std::shared_lock<std::shared_mutex> lock(mtx_img_);
@@ -188,13 +184,10 @@ void StereoVisionProcessor::updateViewer()
     std::this_thread::sleep_for(std::chrono::milliseconds(viewer_update_sleep_));
   }
   cv::destroyAllWindows();
-  up_viewer_ = false;
-  stop_viewer_ = false;
 }
 
 void StereoVisionProcessor::updatePcdViewer()
 {
-  up_pcd_viewer_ = true;
   pcl::visualization::PCLVisualizer viewer("3D viewer");
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcd(new pcl::PointCloud<pcl::PointXYZRGB>);
 
@@ -202,7 +195,7 @@ void StereoVisionProcessor::updatePcdViewer()
   viewer.addCoordinateSystem(coordinate_system_);
   viewer.initCameraParameters();
 
-  while (!stop_pcd_viewer_)
+  while (!stop_pcd_viewer_thread_.load())
   {
     {
       std::shared_lock<std::shared_mutex> lock(mtx_pcd_);
@@ -218,10 +211,7 @@ void StereoVisionProcessor::updatePcdViewer()
     }
     viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, points_size_, "pcd");
     viewer.spinOnce(viewer_update_sleep_);
-    std::this_thread::sleep_for(std::chrono::milliseconds(img_update_sleep_));
   }
-  up_pcd_viewer_ = false;
-  stop_pcd_viewer_ = false;
 }
 
 bool StereoVisionProcessor::run(int video_cap)
@@ -231,23 +221,21 @@ bool StereoVisionProcessor::run(int video_cap)
     return false;
   }
 
-  img_ = cv::Mat::zeros(cv::Size(img_size_.width*2, img_size_.height), CV_8UC3);
-  left_ = cv::Mat::zeros(img_size_, CV_8UC3);
-  right_ = cv::Mat::zeros(img_size_, CV_8UC3);
-  disp_ = cv::Mat::zeros(img_size_, CV_16S);
-  rgb_ = cv::Mat::zeros(img_size_, CV_8UC3);
+  img_    = cv::Mat::zeros(cv::Size(img_size_.width*2, img_size_.height), CV_8UC3);
+  left_   = cv::Mat::zeros(img_size_, CV_8UC3);
+  right_  = cv::Mat::zeros(img_size_, CV_8UC3);
+  disp_   = cv::Mat::zeros(img_size_, CV_16S);
+  rgb_    = cv::Mat::zeros(img_size_, CV_8UC3);
 
   // launch image update thread first
   std::chrono::milliseconds timeout(5000);
   auto start = std::chrono::high_resolution_clock::now();
-  video_cap_ = false;
-  th_img_update_ = std::thread(&StereoVisionProcessor::updateImage, this, video_cap);
-  while (!video_cap_)
+
+  is_video_cap_open_ = false;
+  stop_img_update_thread_.store(false);
+  fut_img_update_thread_ = std::async(std::launch::async, &StereoVisionProcessor::updateImage, this, video_cap);
+  while (!is_video_cap_open_.load())
   {
-    if (th_img_update_.joinable())
-    {
-      th_img_update_.detach();
-    }
     auto now = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - start);
     if (elapsed >= timeout)
@@ -260,22 +248,22 @@ bool StereoVisionProcessor::run(int video_cap)
   std::cout << "Image Update thread is running..." << std::endl;
 
   // disparity thread
-  th_disp_ = std::thread(&StereoVisionProcessor::computeDisparity, this);
-  th_disp_.detach();
+  stop_disp_thread_.store(false);
+  fut_disp_thread_ = std::async(std::launch::async, &StereoVisionProcessor::computeDisparity, this);
   std::cout << "Disparity compute thread is running..." << std::endl;
 
   // image viewer thread
-  th_viewer_ = std::thread(&StereoVisionProcessor::updateViewer, this);
-  th_viewer_.detach();
+  stop_viewer_thread_.store(false);
+  fut_viewer_thread_ = std::async(std::launch::async, &StereoVisionProcessor::updateViewer, this);
   std::cout << "Viewer update thread is running..." << std::endl;
 
   // pcd generator & pcd viewer thread
   if (point_cloud_viewer_)
   {
-    th_pcd_ = std::thread(&StereoVisionProcessor::computePointCloud, this);
-    th_pcd_.detach();
-    th_pcd_viewer_ = std::thread(&StereoVisionProcessor::updatePcdViewer, this);
-    th_pcd_viewer_.detach();
+    stop_pcd_thread_.store(false);
+    fut_pcd_thread_ = std::async(std::launch::async, &StereoVisionProcessor::computePointCloud, this);
+    stop_pcd_viewer_thread_.store(false);
+    fut_pcd_viewer_thread_ = std::async(std::launch::async, &StereoVisionProcessor::updatePcdViewer, this);
     std::cout << "PointCloud threads are running..." << std::endl;
   }
 
@@ -284,30 +272,24 @@ bool StereoVisionProcessor::run(int video_cap)
 
 bool StereoVisionProcessor::stopThreads()
 {
-  if (!stop_img_update_ && !th_img_update_.joinable() && up_img_update_)
-    stop_img_update_ = true;
-  else
-    stop_img_update_ = false;
-  if (!stop_disp_ && !th_disp_.joinable() && up_disp_)
-    stop_disp_ = true;
-  else
-    stop_disp_ = false;
-  if (!stop_pcd_ && !th_pcd_.joinable() && up_pcd_)
-    stop_pcd_ = true;
-  else
-    stop_pcd_ = false;
-  if (!stop_viewer_ && !th_viewer_.joinable() && up_viewer_)
-    stop_viewer_ = true;
-  else
-    stop_viewer_ = false;
-  if (!stop_pcd_viewer_ && !th_pcd_viewer_.joinable() && up_pcd_viewer_)
-    stop_pcd_viewer_ = true;
-  else
-    stop_pcd_viewer_ = false;
+  #define CHECK_THREAD_AND_STOP(thread_name) \
+    if (!stop_##thread_name##_thread_.load() && \
+        fut_##thread_name##_thread_.wait_for(0ms) == std::future_status::timeout) \
+      stop_##thread_name##_thread_.store(true)
+
+  CHECK_THREAD_AND_STOP(img_update);
+  CHECK_THREAD_AND_STOP(disp);
+  CHECK_THREAD_AND_STOP(pcd);
+  CHECK_THREAD_AND_STOP(viewer);
+  CHECK_THREAD_AND_STOP(pcd_viewer);
 
   std::chrono::milliseconds timeout(5000);
   auto start = std::chrono::high_resolution_clock::now();
-  while (stop_img_update_ || stop_disp_ || stop_pcd_ || stop_viewer_ || stop_pcd_viewer_)
+  while (fut_img_update_thread_.wait_for(0ms) != std::future_status::ready ||
+         fut_disp_thread_.wait_for(0ms)       != std::future_status::ready ||
+         fut_pcd_thread_.wait_for(0ms)        != std::future_status::ready ||
+         fut_viewer_thread_.wait_for(0ms)     != std::future_status::ready ||
+         fut_pcd_viewer_thread_.wait_for(0ms) != std::future_status::ready)
   {
     auto now = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - start);
